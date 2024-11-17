5cd4bf94c8931689f1642a7208f0bf43b6131b3e
AlekseySapi <altmind92@gmail.com>
2024-11-17 21:59:29 +0300

    Убрал лишнее

--- app/src/main/java/ru/sapiapps/randomword/MainActivity.kt
+++ app/src/main/java/ru/sapiapps/randomword/MainActivity.kt
@@ -1,39 +1,11 @@
 package ru.sapiapps.randomword
 
 import android.os.Bundle
-import android.widget.CheckBox
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import ru.sapiapps.randomword.ui.theme.RandomWordTheme
-import android.widget.ToggleButton
-import androidx.appcompat.app.AppCompatActivity
 import ru.sapiapps.randomword.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    // Активные категории для генерации слов
-    private val activeCategories = mutableListOf<List<String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
